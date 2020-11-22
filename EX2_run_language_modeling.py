d7c4b05fbb92be7e11af17e70fde91ca3086adb4
lourdescrivelli <58345268+lourdescrivelli@users.noreply.github.com>
2020-11-22 11:54:05 -0500

    Update EX2_run_language_modeling.py

--- EX2_run_language_modeling.py
+++ EX2_run_language_modeling.py
@@ -338,6 +338,7 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
             model.train()
             outputs = model(inputs, masked_lm_labels=labels) if args.mlm else model(inputs, labels=labels)
             loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
+            print("Training loss : ",loss)
 
             if args.n_gpu > 1:
                 loss = loss.mean()  # mean() to average on multi-gpu parallel training
